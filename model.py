1133171b1c07efc3d67a9d8e042d1a7c6c9c3cec
Nikolay Falaleev <nikolasent@gmail.com>
2018-05-11 01:43:35 +0300

    feat: Add the predict script and the first model
    feat: Basic predict implementation
    feat: Add LinkNet34 model

---
+++ model.py
@@ -0,0 +1,107 @@
+"""
+From https://github.com/asanakoy/kaggle_carvana_segmentation
+all credits to @nizhib
+"""
+import torch
+import torch.nn as nn
+from torchvision import models
+import torch.nn.functional as F
+import math
+import torch.utils.model_zoo as model_zoo
+
+nonlinearity = nn.ReLU
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, n_filters):
+        super().__init__()
+
+        # B, C, H, W -> B, C/4, H, W
+        self.conv1 = nn.Conv2d(in_channels, in_channels // 4, 1)
+        self.norm1 = nn.BatchNorm2d(in_channels // 4)
+        self.relu1 = nonlinearity(inplace=True)
+
+        # B, C/4, H, W -> B, C/4, H, W
+        self.deconv2 = nn.ConvTranspose2d(in_channels // 4, in_channels // 4, 3,
+                                          stride=2, padding=1, output_padding=1)
+        self.norm2 = nn.BatchNorm2d(in_channels // 4)
+        self.relu2 = nonlinearity(inplace=True)
+
+        # B, C/4, H, W -> B, C, H, W
+        self.conv3 = nn.Conv2d(in_channels // 4, n_filters, 1)
+        self.norm3 = nn.BatchNorm2d(n_filters)
+        self.relu3 = nonlinearity(inplace=True)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.relu1(x)
+        x = self.deconv2(x)
+        x = self.norm2(x)
+        x = self.relu2(x)
+        x = self.conv3(x)
+        x = self.norm3(x)
+        x = self.relu3(x)
+        return x
+
+class LinkNet34(nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super().__init__()
+        assert input_channels > 0
+        filters = [64, 128, 256, 512]
+        resnet = models.resnet34(pretrained=False)
+
+        #if input_channels != 3:
+        resnet.conv1 = nn.Conv2d(input_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(7, 3), bias=False)
+
+        self.firstconv = resnet.conv1
+        self.firstbn = resnet.bn1
+        self.firstrelu = resnet.relu
+        self.firstmaxpool = resnet.maxpool
+        self.encoder1 = resnet.layer1
+        self.encoder2 = resnet.layer2
+        self.encoder3 = resnet.layer3
+        self.encoder4 = resnet.layer4
+
+        # Decoder
+        self.decoder4 = DecoderBlock(filters[3], filters[2])
+        self.decoder3 = DecoderBlock(filters[2], filters[1])
+        self.decoder2 = DecoderBlock(filters[1], filters[0])
+        self.decoder1 = DecoderBlock(filters[0], filters[0])
+
+        # Final Classifier
+        self.finaldeconv1 = nn.ConvTranspose2d(filters[0], 32, 3, stride=2)
+        self.finalrelu1 = nonlinearity(inplace=True)
+        self.finalconv2 = nn.Conv2d(32, 32, 7, padding=(0,3))
+        self.finalrelu2 = nonlinearity(inplace=True)
+        self.finalconv3 = nn.Conv2d(32, 32, 3)
+        self.finalrelu3 = nonlinearity(inplace=True)
+        self.finalconv4 = nn.Conv2d(32, output_channels, 2, padding=(0,1))
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # Encoder
+        x = self.firstconv(x)
+        x = self.firstbn(x)
+        x = self.firstrelu(x)
+        x = self.firstmaxpool(x)
+        e1 = self.encoder1(x)
+        e2 = self.encoder2(e1)
+        e3 = self.encoder3(e2)
+        e4 = self.encoder4(e3)
+
+        # Decoder with Skip Connections
+        d4 = self.decoder4(e4) + e3
+        d3 = self.decoder3(d4) + e2
+        d2 = self.decoder2(d3) + e1
+        d1 = self.decoder1(d2)
+
+        # Final Classification
+        x = self.finaldeconv1(d1)
+        x = self.finalrelu1(x)
+        x = self.finalconv2(x)
+        x = self.finalrelu2(x)
+        x = self.finalconv3(x)
+        x = self.finalrelu3(x)
+        x = self.finalconv4(x)
+        return self.sigmoid(x)
---
+++ predict.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import cv2
+import time
+import torch
+import numpy as np
+import skvideo.io
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+
+from io import BytesIO, StringIO
+from PIL import Image
+import sys, json, base64
+
+from model import LinkNet34
+
+
+v_file = sys.argv[-1]
+model_path = './data/models/resnet34_001.pth'
+
+batch = 8
+THRES_VEH = 0.5
+THRES_ROAD = 0.5
+
+def encode(array):
+    pil_img = Image.fromarray(array)
+    buff = BytesIO()
+    pil_img.save(buff, format="PNG")
+    return base64.b64encode(buff.getvalue()).decode("utf-8") 
+
+class LyftTestDataset(Dataset):
+    def __init__(self, v_file, img_transform=None):
+        self.video = skvideo.io.vread(v_file)
+        self.img_transform = img_transform
+
+    def __len__(self):
+        return self.video.shape[0]
+
+    def __getitem__(self, idx):
+        img = self.video[idx, :, :, :]
+        if self.img_transform is not None:
+            img = self.img_transform(img)
+        return img
+
+test_dataset = LyftTestDataset(v_file, transforms.ToTensor())
+test_loader = DataLoader(test_dataset, batch_size=batch, shuffle=False)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = LinkNet34(3, 3).to(device)
+state = torch.load(model_path)
+model.load_state_dict(state)
+
+answer_key = {}
+frame = 1
+for data in test_loader:
+    pred = model(data.type(torch.cuda.FloatTensor))
+    pred = pred.cpu().data.numpy()
+    for i in range(pred.shape[0]):
+        img = pred[i, :, :, :]
+        binary_car_result = np.where(img[2,:,:]>THRES_VEH,1,0).astype('uint8')
+        binary_road_result = np.where(img[1,:,:]>THRES_ROAD,1,0).astype('uint8')
+        answer_key[frame] = [encode(binary_car_result), encode(binary_road_result)]
+        frame+=1
+
+print (json.dumps(answer_key)) 
