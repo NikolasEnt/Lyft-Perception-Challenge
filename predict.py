d0994e0304465111b33f61e0a9e12b9719787186
Nikolay Falaleev <nikolasent@gmail.com>
2018-05-12 16:21:18 +0300

    feat: Update predict to fit the new padded model

--- predict.py
+++ predict.py
@@ -16,7 +16,7 @@ from model import LinkNet34
 
 
 v_file = sys.argv[-1]
-model_path = './data/models/resnet34_001.pth'
+model_path = './data/models/resnet34_002_cpt.pth'
 
 batch = 8
 THRES_VEH = 0.5
@@ -38,6 +38,7 @@ class LyftTestDataset(Dataset):
 
     def __getitem__(self, idx):
         img = self.video[idx, :, :, :]
+        img = cv2.copyMakeBorder(img,4,4,0,0,cv2.BORDER_REFLECT)
         if self.img_transform is not None:
             img = self.img_transform(img)
         return img
@@ -55,10 +56,14 @@ for data in test_loader:
     pred = model(data.type(torch.cuda.FloatTensor))
     pred = pred.cpu().data.numpy()
     for i in range(pred.shape[0]):
-        img = pred[i, :, :, :]
+        img = pred[i, :, 4:604, :]
+        print(img.shape)
         binary_car_result = np.where(img[2,:,:]>THRES_VEH,1,0).astype('uint8')
         binary_road_result = np.where(img[1,:,:]>THRES_ROAD,1,0).astype('uint8')
         answer_key[frame] = [encode(binary_car_result), encode(binary_road_result)]
         frame+=1
 
-print (json.dumps(answer_key)) 
+print (json.dumps(answer_key))
+
+#with open('preds.json', 'w') as outfile:
+    #json.dump(answer_key, outfile)
